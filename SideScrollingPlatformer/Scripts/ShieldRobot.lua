30559d93f1a7a44e6f03e9ecc1ca53e80787fd17
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2022-06-05 18:29:08 +0100

    put all the robot sprites onto one sprite sheet. Added a robot lua script

---
+++ SideScrollingPlatformer/Scripts/ShieldRobot.lua
@@ -0,0 +1,47 @@
+local rigidBody = {}
+local transform = {}
+local sprite = {}
+local count = 1
+local backwards = false
+local attacking = false
+
+-- Called when entity is created
+function OnCreate()
+	rigidBody = Entity:GetOrAddRigidBody2DComponent()
+	transform = Entity:GetTransformComponent()
+	sprite = Entity:GetAnimatedSpriteComponent()
+end
+
+-- Called once per frame
+function OnUpdate(deltaTime)
+
+end
+-- Called on a fixed interval
+function OnFixedUpdate()
+	if(not backwards and not attacking) then
+		rigidBody:ApplyForce(Vec2.new(40000.0, 0.0))
+	elseif (not attacking) then
+		rigidBody:ApplyForce(Vec2.new(-40000.0, 0.0))
+	end
+
+	if(count > 200 and not attacking) then
+		attacking = true
+		sprite:SelectAnimation("Unnamed Animation (4)")
+	end
+
+	if(count > 700) then
+		count = 0
+		backwards = not backwards
+		transform.Scale.x = -transform.Scale.x
+		sprite:SelectAnimation("Unnamed Animation (5)")
+		attacking = false
+	else
+		count = count + 1
+	end
+end
+-- Called when entity is destroyed
+function OnDestroy()
+
+end
+
+
---
+++ SideScrollingPlatformer/Textures/Sprites/ShieldRobot.tsx
@@ -0,0 +1,64 @@
+<tileset name="" tilewidth="90" tileheight="31" tilecount="4" columns="2" backgroundcolor="#ff000000">
+    <image source="Shield Robot.png" width="450" height="248"/>
+    <tile id="39">
+        <animation>
+            <frame tileid="39" duration="0.1"/>
+        </animation>
+    </tile>
+    <tile id="16">
+        <animation>
+            <frame tileid="16" duration="0.1"/>
+            <frame tileid="17" duration="0.1"/>
+            <frame tileid="18" duration="0.1"/>
+        </animation>
+    </tile>
+    <tile id="1">
+        <animation>
+            <frame tileid="1" duration="0.1"/>
+            <frame tileid="2" duration="0.1"/>
+            <frame tileid="3" duration="0.1"/>
+            <frame tileid="4" duration="0.1"/>
+            <frame tileid="5" duration="0.1"/>
+            <frame tileid="6" duration="0.1"/>
+            <frame tileid="7" duration="0.1"/>
+            <frame tileid="8" duration="0.1"/>
+            <frame tileid="9" duration="0.1"/>
+            <frame tileid="10" duration="0.1"/>
+        </animation>
+    </tile>
+    <tile id="10">
+        <animation>
+            <frame tileid="10" duration="0.1"/>
+            <frame tileid="11" duration="0.1"/>
+            <frame tileid="12" duration="0.1"/>
+            <frame tileid="13" duration="0.1"/>
+            <frame tileid="14" duration="0.1"/>
+        </animation>
+    </tile>
+    <tile id="27">
+        <animation>
+            <frame tileid="27" duration="0.1"/>
+            <frame tileid="28" duration="0.1"/>
+            <frame tileid="29" duration="0.1"/>
+            <frame tileid="30" duration="0.1"/>
+            <frame tileid="31" duration="0.1"/>
+            <frame tileid="32" duration="0.1"/>
+            <frame tileid="33" duration="0.1"/>
+            <frame tileid="34" duration="0.1"/>
+            <frame tileid="35" duration="0.1"/>
+            <frame tileid="36" duration="0.1"/>
+            <frame tileid="37" duration="0.1"/>
+            <frame tileid="38" duration="0.1"/>
+        </animation>
+    </tile>
+    <tile id="20">
+        <animation>
+            <frame tileid="20" duration="0.1"/>
+            <frame tileid="21" duration="0.1"/>
+            <frame tileid="22" duration="0.1"/>
+            <frame tileid="23" duration="0.1"/>
+            <frame tileid="24" duration="0.1"/>
+            <frame tileid="25" duration="0.1"/>
+        </animation>
+    </tile>
+</tileset>
