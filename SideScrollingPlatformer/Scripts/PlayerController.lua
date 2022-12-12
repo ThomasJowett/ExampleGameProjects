e9c8d221bdebc910543e96906082ca20b58b7246
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2022-12-12 18:19:17 +0000

    Added some code for the side scroller character

--- SideScrollingPlatformer/Scripts/PlayerController.lua
+++ SideScrollingPlatformer/Scripts/PlayerController.lua
@@ -4,6 +4,18 @@ local spriteComp = {}
 local rigidBodyComp = {}
 local collider = {}
 local physicsMaterial = {}
+local transformComp = {}
+
+local isGrounded = false;
+
+function CheckIsGrounded()
+	local hit = CurrentScene:RayCast2D(
+		Vec2.new(transformComp.Position.x, transformComp.Position.y- (collider.Height * transformComp.Scale.y * 0.5)),
+		Vec2.new(transformComp.Position.x, transformComp.Position.y - (collider.Height * transformComp.Scale.y * 0.5) - 1))
+
+	isGrounded = hit.Hit;
+	--Log.Debug(tostring(hit.Hit))
+end
 
 
 -- Called when entity is created
@@ -12,34 +24,52 @@ function OnCreate()
 	rigidBodyComp = CurrentEntity:GetRigidBody2DComponent()
 	collider = CurrentEntity:GetCapsuleCollider2DComponent()
 	physicsMaterial = collider.PhysicsMaterial
+	transformComp = CurrentEntity:GetTransformComponent()
 end
 
 -- Called once per frame
 function OnUpdate(deltaTime)
-
+	
 end
 -- Called on a fixed interval
 function OnFixedUpdate()
-	--rigidBodyComp.
+	CheckIsGrounded()
+	local velocity = rigidBodyComp:GetLinearVelocity()
+	local moveInput = 0.0
+
+	
+physicsMaterial:SetFriction(0.01)
+	
 	if Input.IsKeyPressed('A') then
-		rigidBodyComp:ApplyForce(Vec2.new(-100.0, 0))
-		--physicsMaterial.Friction = 0.01
-	elseif(Input.IsKeyPressed('D')) then
-		rigidBodyComp:ApplyForce(Vec2.new(100.0, 0))
-		--physicsMaterial.Friction = 0.01
-	else
-		--physicsMaterial.Friction = 0.5
+		moveInput = -1.0
+		
 	end
 
-	if Input.IsKeyPressed(' ') then
-		rigidBodyComp:ApplyImpulse(Vec2.new(0, 10.0))
-	end 
+	if (Input.IsKeyPressed('D')) then
+		moveInput = moveInput + 1.0
+	end
 
+	if not isGrounded or moveInput == 0 then
+		physicsMaterial:SetFriction(1.0)
+	else
+		--spriteComp:SelectAnimation("Run")
+	end
+
+
+	rigidBodyComp:SetLinearVelocity(Vec2.new(moveInput * maxSpeed, velocity.y))
+
+	if Input.IsKeyPressed(' ') and isGrounded then
+		rigidBodyComp:ApplyImpulse(Vec2.new(0, 5.0))
+	end 
+	
+	if(rigidBodyComp:GetLinearVelocity().x > 0.0001) then
+		transformComp.Scale.x = math.abs(transformComp.Scale.x)
+	elseif(rigidBodyComp:GetLinearVelocity().x < -0.0001) then
+		transformComp.Scale.x = -math.abs(transformComp.Scale.x)
+	end
 end
 -- Called when entity is destroyed
 function OnDestroy()
 
 end
-
-
 
