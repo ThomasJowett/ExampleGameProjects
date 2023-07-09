cf9f2bc36acabb5b9071f97a35dcaa0eae7bd05e
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2023-07-09 15:43:44 +0100

    Added a best score text that is read from file

--- InfiniteRunner/Scripts/Score.lua
+++ InfiniteRunner/Scripts/Score.lua
@@ -1,6 +1,11 @@
 local timeElapsed = 0.0
 
 local scoreText = {}
+local bestScore = 0.0
+
+local full_screen = false
+
+local filename = App.GetDocumentDirectory() .. "/score.txt"
 
 function FormatTime(t)
 	local minutes = math.floor(t / 60)
@@ -11,6 +16,16 @@ end
 -- Called when entity is created
 function OnCreate()
 	scoreText = CurrentEntity:GetTextComponent()
+
+	local file = io.open(filename, "r")
+
+	if file then
+
+		local contents = file:read("*a")
+
+		bestScore = tonumber(contents)
+		Log.Debug("Best Score: " .. tostring(bestScore))
+	end
 end
 
 -- Called once per frame
@@ -19,8 +34,19 @@ function OnUpdate(deltaTime)
 	timeElapsed = timeElapsed + deltaTime
 
 	local t = FormatTime(timeElapsed)
+	local b = FormatTime(bestScore)
 
-	scoreText.Text = "TIME: " .. t
+	scoreText.Text = "TIME: " .. t .. "\nBEST: " .. b
+
+	if Input.IsKeyPressed('F') then
+		Log.Debug("Pressed")
+		full_screen = not full_screen
+		if full_screen then
+			App.SetWindowMode(WindowMode.Full_Screen)
+		else
+			App.SetWindowMode(WindowMode.Windowed)
+		end
+	end
 end
 -- Called on a fixed interval
 function OnFixedUpdate()
@@ -28,7 +54,17 @@ function OnFixedUpdate()
 end
 -- Called when entity is destroyed
 function OnDestroy()
-
+	if timeElapsed > bestScore then
+		local file = io.open(filename, "w")
+		
+		if file then
+			file:write(tostring(timeElapsed))
+			file:close()
+		else
+			Log.Error("Failed to open file for writing")
+		end
+	end
 end
+
 
 
